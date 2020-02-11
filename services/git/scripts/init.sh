779be53e2e49edd72d696796f832eba35795be49
Zachary Balder <zmbalder@gmail.com>
2020-02-11 15:11:39 -0500

    add readme, touch authorized_keys file in init script

--- services/git/scripts/init.sh
+++ services/git/scripts/init.sh
@@ -5,5 +5,7 @@ cd "$(dirname "$0")"/..
 ssh-keygen -t ed25519 -f etc/ssh/id_ed25519 -N ''
 
 mkdir -p .ssh projects
+touch .ssh/authorized_keys
+
 sudo chown -R "$USER":"$USER" .ssh
 sudo chown -R "$USER":"$USER" projects
